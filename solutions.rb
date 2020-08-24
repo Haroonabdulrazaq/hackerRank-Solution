ba329072826fa5b17b127f432bc20772cff4f562
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-24 11:27:19 +0100

    fizzbuzz solution

--- solutions.rb
+++ solutions.rb
@@ -1,4 +1,16 @@
-#FizzBuzz
+# FizzBuzz
+100.times do |i|
+    i+=1
+    if i%15==0
+        puts "FizzBuzz"
+    elsif i%5==0
+        puts "Buzz"
+    elsif i%3==0
+        puts "Fizz"
+    else
+        puts i
+    end
+end
 
 # Repeated String
 
