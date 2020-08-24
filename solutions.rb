bd83a796bb761dfa70717ac8f6aea51d78b5b832
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-24 11:23:36 +0100

    Add Repeated String solution

--- solutions.rb
+++ solutions.rb
@@ -1,2 +1,9 @@
 # Repeated String
 
+def repeatedString(s, n) 
+    remainder_count = s[0,(n % s.length)].count "a"
+    whole_count =  (s.count 'a') * (n / s.length) 
+    whole_count + remainder_count
+end
+
+puts repeatedString('aba', 10)
