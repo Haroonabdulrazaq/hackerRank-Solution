9fa4d84ac57e695cee26751ff46acc6c7ea14c8a
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-01 23:03:42 +0100

    Add algorithm for the solution

--- solutions.rb
+++ solutions.rb
@@ -54,3 +54,16 @@ if s.length.odd?
     end
 
     repeatedString('abcac', 10)
+
+
+    #Algorithm
+#init_value is 3
+#start_time is 1
+#count down the init_time
+#Increase the start_time
+#If init_time is 1 double the init_time
+#Keep doing until start_time == t
+#return init_value
+
+#MY CODE BUT NOT EFFICIENT FOR LARGE NUMBERS
+# USE GEO-METRIC PROGREESION INSTEAD
