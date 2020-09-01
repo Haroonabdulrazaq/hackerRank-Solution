3aa606397d095f5730eefb8dbd72939351088889
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-01 23:04:35 +0100

    Add algorithm for the solution

--- solutions.rb
+++ solutions.rb
@@ -67,3 +67,20 @@ if s.length.odd?
 
 #MY CODE BUT NOT EFFICIENT FOR LARGE NUMBERS
 # USE GEO-METRIC PROGREESION INSTEAD
+def strangeCounter(t)
+  init_value = 3
+  start_time = 1
+  temp = init_value
+  loop_time =t-1
+  loop_time.times do |i|
+    init_value -= 1
+    start_time += 1
+    if init_value == 0
+      init_value = temp * 2
+      temp = init_value
+    end
+    
+  end
+  p "Init_value #{init_value}"
+end
+strangeCounter(10)
