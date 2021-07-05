c3711e07b0c1b80c8d483e294bcbabd936d0eabc
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-07-05 16:09:24 +0100

    Add solution to VerybigSum

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-function compareTriplets(a, b) {
-  // Write your code
-  let first=0;
-  let second=0;
-  for(let i=0; i< a.length; i++) {
-    if (a[i] > b[i]) {
-      first++
-    }else if (a[i] === b[i])  {
-      first + 0
-      second + 0
-    }else {
-      second++
-    }
-  }
-
-  return [first, second];
+function aVeryBigSum(ar) {
+  // Write your code here
+  return ar.reduce((a, b) => a + b);
 }
-console.log(compareTriplets([17, 28, 30], [99, 16, 8]));
+
+console.log(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]));
