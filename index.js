001b95fa1ba05172f178c1c233218e0832d434c7
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-07-05 17:51:06 +0100

    Solve DiagonalDifference

--- index.js
+++ index.js
@@ -11,9 +11,3 @@ function diagonalDifference(arr) {
   return Math.abs(firstArr.reduce((a,b )=> a+ b) - secondArr.reduce((a,b )=> a+ b))
 }
 console.log(diagonalDifference([[11, 2, 4],[4, 5, 6], [10, 8, -12]]));
-// 11 2 4
-// 4 5 6
-// 10 8 -12
-
-11, 5, -12
-4, 5, 10 
