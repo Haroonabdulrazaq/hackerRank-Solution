9c389a3be747d098eea4191331940118a15254bc
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-07-05 16:05:49 +0100

    Add soluion for compareTriplet

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function simpleArraySum(ar) {
-  // Write your code here
-  return ar.reduce((a,b) =>  a + b);
+function compareTriplets(a, b) {
+  // Write your code
+  let first=0;
+  let second=0;
+  for(let i=0; i< a.length; i++) {
+    if (a[i] > b[i]) {
+      first++
+    }else if (a[i] === b[i])  {
+      first + 0
+      second + 0
+    }else {
+      second++
+    }
+  }
+
+  return [first, second];
 }
-console.log(simpleArraySum([1, 2, 3, 4, 10, 11]));
+console.log(compareTriplets([17, 28, 30], [99, 16, 8]));
