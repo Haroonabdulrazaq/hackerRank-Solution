4245835dd333d6a0acd63db0c8bac2ba4a4ff6d3
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-07-06 17:29:47 +0100

    Add BirthdayCakeCandle problem to Solution file[D

--- index.js
+++
@@ -1,8 +0,0 @@
-function birthdayCakeCandles(candles) {
-  const max = Math.max(...candles);
-  const result = candles.filter((ele) => ele == max);
-  return result.length;
-}
-
-
-console.log(birthdayCakeCandles([3, 3,2, 1, 3]));
--- solutions.js
+++ solutions.js
@@ -89,3 +89,14 @@ function miniMaxSum(arr) {
   console.log((result - max), (result - min));
 }
 console.log(miniMaxSum([1,2,3,4,5]))
+
+
+// BirthDay Caskes Candle
+function birthdayCakeCandles(candles) {
+  const max = Math.max(...candles);
+  const result = candles.filter((ele) => ele == max);
+  return result.length;
+}
+
+
+console.log(birthdayCakeCandles([3, 3,2, 1, 3]));
