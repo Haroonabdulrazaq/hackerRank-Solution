ca02f4679f58c457ab5791df5d94227880d515a2
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-07-06 14:43:31 +0100

    Add solution to PlusMinus to Solution file

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-function plusMinus(arr) {
-  let posCounter=0;
-  let negCounter=0;
-  let zeroCounter=0;
-  for (let i=0; i< arr.length; i++) {
-    if(arr[i] > 0) {
-      posCounter++;
-    } else if(arr[i] < 0) {
-      negCounter++;
-    }else {
-      zeroCounter++
-    }
-  }
-  console.log((posCounter/arr.length).toFixed(6)) 
-  console.log((negCounter/arr.length).toFixed(6))
-   console.log((zeroCounter/arr.length).toFixed(6))
+function staircase(n) {
+  // Write your code here
+
 }
-console.log(plusMinus([-4, 3, -9, 0, 4, 1]));
--- solutions.js
+++ solutions.js
@@ -48,3 +48,24 @@ function diagonalDifference(arr) {
   return Math.abs(firstArr.reduce((a,b )=> a+ b) - secondArr.reduce((a,b )=> a+ b))
 }
 console.log(diagonalDifference([[11, 2, 4],[4, 5, 6], [10, 8, -12]]));
+
+// Plus and Minus
+function plusMinus(arr) {
+  let posCounter=0;
+  let negCounter=0;
+  let zeroCounter=0;
+  for (let i=0; i< arr.length; i++) {
+    if(arr[i] > 0) {
+      posCounter++;
+    } else if(arr[i] < 0) {
+      negCounter++;
+    }else {
+      zeroCounter++
+    }
+  }
+  console.log((posCounter/arr.length).toFixed(6)) 
+  console.log((negCounter/arr.length).toFixed(6))
+   console.log((zeroCounter/arr.length).toFixed(6))
+}
+console.log(plusMinus([-4, 3, -9, 0, 4, 1]));
+
