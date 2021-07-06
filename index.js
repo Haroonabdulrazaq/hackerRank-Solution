962dff443969b83c2ff50edbc48e56da91ddcf1b
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-07-06 14:41:53 +0100

    Add solution to PlusMinus

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-function diagonalDifference(arr) {
-  let counter = arr.length-1;
-  const firstArr=[];
-  const secondArr=[];
-  for(let i = 0; i < arr.length; i++) {
-   firstArr.push(arr[i][i])
-   secondArr.push(arr[i][counter])
-    counter--;
+function plusMinus(arr) {
+  let posCounter=0;
+  let negCounter=0;
+  let zeroCounter=0;
+  for (let i=0; i< arr.length; i++) {
+    if(arr[i] > 0) {
+      posCounter++;
+    } else if(arr[i] < 0) {
+      negCounter++;
+    }else {
+      zeroCounter++
+    }
   }
-
-  return Math.abs(firstArr.reduce((a,b )=> a+ b) - secondArr.reduce((a,b )=> a+ b))
+  console.log((posCounter/arr.length).toFixed(6)) 
+  console.log((negCounter/arr.length).toFixed(6))
+   console.log((zeroCounter/arr.length).toFixed(6))
 }
-console.log(diagonalDifference([[11, 2, 4],[4, 5, 6], [10, 8, -12]]));
+console.log(plusMinus([-4, 3, -9, 0, 4, 1]));
--- solutions.js
+++ solutions.js
@@ -47,3 +47,4 @@ function diagonalDifference(arr) {
 
   return Math.abs(firstArr.reduce((a,b )=> a+ b) - secondArr.reduce((a,b )=> a+ b))
 }
+console.log(diagonalDifference([[11, 2, 4],[4, 5, 6], [10, 8, -12]]));
