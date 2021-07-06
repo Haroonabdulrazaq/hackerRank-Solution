61ab8de3a5e561ca6258f1c721086ef346eda3e2
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-07-06 17:15:56 +0100

    Add MinMax problem to Solution file

--- index.js
+++
@@ -1,8 +0,0 @@
-function miniMaxSum(arr) {
-  // Write your code here
-  const min = Math.min(...arr);
-  const max = Math.max(...arr);
-  const result = arr.reduce((a,b) =>  a+b);
-  console.log((result - max), (result - min));
-}
-console.log(miniMaxSum([1,2,3,4,5]))
--- solutions.js
+++ solutions.js
@@ -79,3 +79,13 @@ function staircase(n) {
 
 }
 staircase(6);
+
+// MinMax Problem
+function miniMaxSum(arr) {
+  // Write your code here
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const result = arr.reduce((a,b) =>  a+b);
+  console.log((result - max), (result - min));
+}
+console.log(miniMaxSum([1,2,3,4,5]))
