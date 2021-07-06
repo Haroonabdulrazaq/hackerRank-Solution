9d2bbff3e1ce19f4c273cc14c677cf1c30b3e3a9
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-07-06 16:57:52 +0100

    Add solution to StairCase problem

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function staircase(n) {
-  // Write your code here
+  let count = n;
+  count -= 1
+  for(let i = 1 ; i <= n; i++) {
+    console.log("#".repeat(i).padStart(n))
+  }
 
 }
+staircase(6);
