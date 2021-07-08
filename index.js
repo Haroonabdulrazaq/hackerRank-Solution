555b29ed3d66cd40d39a22db87b2227c46505303
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-07-08 15:29:21 +0100

    Add countApplesAndOranges to solution file

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
  
-  const samApples = apples.map((ele) => a + ele )
-        .filter((ele) => ele>=s && ele <= t )
-  const samOranges = oranges.map((ele) => b + ele )
-        .filter((ele) => ele>=s && ele <= t )
-
-        console.log(samApples.length);
-        console.log(samOranges.length);
-
+    console.log(apples.map((ele) => a + ele )
+        .filter((ele) => ele>=s && ele <= t ).length)
+    console.log(oranges.map((ele) => b + ele )
+        .filter((ele) => ele>=s && ele <= t ).length)
 }
 countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
 
--- solutions.js
+++ solutions.js
@@ -156,3 +156,18 @@ function timeConversion(s) {
  // Subtract the remainder from what you got
  // If it is less than 3 round up
  // else round down or leave as it is
+
+ function countApplesAndOranges(s, t, a, b, apples, oranges) {
+ 
+  console.log(apples.map((ele) => a + ele )
+      .filter((ele) => ele>=s && ele <= t ).length)
+  console.log(oranges.map((ele) => b + ele )
+      .filter((ele) => ele>=s && ele <= t ).length)
+}
+countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]);
+
+
+// Sams house range 7-11
+// a,b Position of first tree and second Tree
+// Apples array
+//Oranges array
